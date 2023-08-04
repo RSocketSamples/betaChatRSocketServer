7deba5957540091c6810a40027a2b30bb6fd8364
bramenn <bramendev@gmail.com>
2023-08-04 11:35:50 -0500

    Dependencies routes updated

--- src/main/java/com/betaChat/chatServer/controller/MessageController.java
+++ src/main/java/com/betaChat/chatServer/controller/MessageController.java
@@ -1,10 +1,11 @@
 package com.betaChat.chatServer.controller;
 
-import com.betaChat.chatServer.dto.Message;
-import com.betaChat.chatServer.dto.MessageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+
+import com.betaChat.chatServer.dto.MessageRequest;
+import com.betaChat.chatServer.entity.Message;
 import com.betaChat.chatServer.service.MessageService;
 import reactor.core.publisher.Mono;
 
--- src/main/java/com/betaChat/chatServer/repository/MessageRepository.java
+++ src/main/java/com/betaChat/chatServer/repository/MessageRepository.java
@@ -1,6 +1,8 @@
-package com.betaChat.chatServer.dto;
+package com.betaChat.chatServer.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.betaChat.chatServer.entity.Message;
 
 public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
 }
